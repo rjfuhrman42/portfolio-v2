9dd366eebc855cf88500385c0685b800118bf87d
Reid Fuhrman <reidjf77@gmail.com>
2021-08-18 16:41:20 -0400

    Contact page is nearer to design

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,29 +2,37 @@ import React, { useContext } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
-import Title from '../Title/Title';
+
+import stars from '../../images/stars.png';
 
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
-  const { cta, btn, email } = contact;
+  const { email } = contact;
 
   return (
     <section id="contact">
       <Container>
-        <Title title="Contact" />
+        {/* <Title title="Contact" /> */}
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
-            <p className="contact-wrapper__text">
+            {/* <p className="contact-wrapper__text">
               {cta || 'Would you like to work with me? Awesome!'}
-            </p>
+            </p> */}
+            <div className="stars-container">
+              <img src={stars} alt="a bunch of purple stars" className="purple-stars" />
+            </div>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
+              className="contact-me"
               href={`mailto:${email}`}
             >
-              {btn || "Let's Talk"}
+              {/* {btn || 'Contact Me'} */}
+              <h2>Contact Me</h2>
             </a>
+            <div className="stars-container">
+              <img src={stars} alt="a bunch of purple stars" className="purple-stars" />
+            </div>
           </div>
         </Fade>
       </Container>
