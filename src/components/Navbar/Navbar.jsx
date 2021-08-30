c27eaeb284760bc8e7892daf648253bbe63c7740
Reid Fuhrman <reidjf77@gmail.com>
2021-08-30 12:40:07 -0400

    Navbar now changes color based on position, Hero page is black again.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Squash as Hamburger } from 'hamburger-react';
 import { Link } from 'gatsby';
 import PortfolioContext from '../../context/context';
@@ -9,6 +9,7 @@ function Navbar() {
   const { navbar } = useContext(PortfolioContext);
   const { navlinks } = navbar;
   const [isOpen, setIsOpen] = useState(false);
+  const [isPastHeroSection, setIsPastHeroSection] = useState(false);
 
   const ResumeLink = (
     <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
@@ -22,6 +23,19 @@ function Navbar() {
 
     setIsOpen((prev) => !prev);
   }
+
+  function handleScroll() {
+    const hero = document.querySelector('#hero');
+    const nav = document.querySelector('#nav-bar');
+    const bottomOfHero = hero.getBoundingClientRect().bottom;
+    const bottomOfNav = nav.getBoundingClientRect().bottom;
+    if (bottomOfNav >= bottomOfHero && isPastHeroSection === false) setIsPastHeroSection(true);
+    else setIsPastHeroSection(false);
+  }
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => handleScroll());
+  }, []);
 
   // useEffect(() => {
   //   // document is not available during server-side rendering, so put in a useEffect()
@@ -39,7 +53,7 @@ function Navbar() {
   // }, []);
 
   return (
-    <nav id="nav-bar">
+    <nav id="nav-bar" className={`nav-bar ${isPastHeroSection ? 'color-change' : ''}`}>
       <Hamburger
         toggled={isOpen}
         toggle={() => handleClick()}
