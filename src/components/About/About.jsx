dcb63c02271436c9ad5b2b8e4b009dd9eec51930
Reid Fuhrman <reidjf77@gmail.com>
2021-08-30 17:00:58 -0400

    Updated the about me design

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { img, paragraphOne, paragraphTwo, paragraphThree } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -25,18 +25,17 @@ const About = () => {
   return (
     <section id="about">
       <Container>
-        <Title title="About Me" />
-        <Row className="about-wrapper">
-          <Col md={6} sm={12}>
-            <Fade bottom duration={1000} delay={600} distance="30px">
+        <div className="palette-rect light" />
+        <div className="palette-rect medium" />
+        <div className="palette-rect dark" />
+        <div className="about-me-flex-container">
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={600} distance="30px">
+            <div className="about-wrapper">
               <div className="about-wrapper__image">
                 <AboutImg alt="profile picture" filename={img} />
               </div>
-            </Fade>
-          </Col>
-          <Col md={6} sm={12}>
-            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
+                <Title title="About Me" />
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
                     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
@@ -48,22 +47,13 @@ const About = () => {
                 <p className="about-wrapper__info-text">
                   {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
                 </p>
-                {resume && (
-                  <span className="d-flex mt-3">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--resume"
-                      href={resume}
-                    >
-                      Resume
-                    </a>
-                  </span>
-                )}
               </div>
-            </Fade>
-          </Col>
-        </Row>
+            </div>
+          </Fade>
+        </div>
+        <div className="palette-rect dark" />
+        <div className="palette-rect medium" />
+        <div className="palette-rect light" />
       </Container>
     </section>
   );
