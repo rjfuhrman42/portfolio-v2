b88f5e33ef2329c0f3af4f96ed30f42c3dea5fe6
Reid Fuhrman <reidjf77@gmail.com>
2021-09-12 18:10:10 -0400

    removed old media, changed some config settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Reid Fuhrman | Developer`,
+        short_name: `Reid Fuhrman`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
