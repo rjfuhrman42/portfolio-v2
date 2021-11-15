4bd89f3aa7152cdbb22bd6011fe7e71dc0b83e5c
Reid Fuhrman <reidjf77@gmail.com>
2021-11-15 00:13:54 -0500

    debugging

--- src/takeNewScreenshot.js
+++ src/takeNewScreenshot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   const page = await browser.newPage();
 
   page.setViewport({
